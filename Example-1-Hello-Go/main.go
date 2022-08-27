df07f72998de92215d9f9e88c4a136a715e353cb
ysfaltn02 <yusuf@bereketaltun.com>
2022-08-28 01:51:49 +0300

    Hello Go !

---
+++ Example-1-Hello-Go/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt" // ---> It means 'Format'
+
+func main() {
+	fmt.Println("Hello Go!") // ---> Println is used to print something on the screen and move to the bottom line.
+}
